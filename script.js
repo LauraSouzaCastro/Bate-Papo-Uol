b5bd567f1741f4b0625835ce193634f7c6ee1bab
LauraSouzaCastro <laurasouzacastro1@gmail.com>
2022-10-26 23:33:03 -0300

    enviar mensagem para todos

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let nome;
 function inicializaPagina(){
     nome = {name : prompt("Qual o seu nome?")};
-    // while(){
-    //     nome = prompt("Qual o seu nome?");
-    // }
-    atualizaMensagens();
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome);
+    promessa.then(sucesso);
+    promessa.catch(falha);
 }
+function sucesso(resposta) {
+	atualizaMensagens();
+}
+function falha(erro) {
+    inicializaPagina();
+}
+function mantemConexao(){
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome);
+}
+setInterval(mantemConexao, 5000);
 inicializaPagina();
 function atualizaMensagens(){
     const mensagem = document.querySelector(".container");
@@ -16,9 +25,7 @@ function atualizaMensagens(){
 setInterval(atualizaMensagens, 3000);
 function processarMensagens(resposta){
     let listaMensagens = resposta.data;
-    console.log(listaMensagens);
     const mensagem = document.querySelector(".container");
-    console.log(mensagem);
     for(let i = 0; i < listaMensagens.length; i++){
         if(listaMensagens[i].type === "status"){
             mensagem.innerHTML += `
@@ -38,7 +45,7 @@ function processarMensagens(resposta){
                     <span class="texto">${listaMensagens[i].text}</span>
                 </div>
             `;
-        }else if(listaMensagens[i].to === nome){
+        }else if(listaMensagens[i].type === "private_message" && listaMensagens[i].to === nome.name){
             mensagem.innerHTML += `
                 <div class="mensagem reservadas" id="${i}">
                     <span class="horario">(${listaMensagens[i].time})</span>
@@ -51,6 +58,17 @@ function processarMensagens(resposta){
         }
     }
     const elementoQueQueroQueApareca = document.getElementById(`${listaMensagens.length-1}`);
-    console.log(elementoQueQueroQueApareca);
     elementoQueQueroQueApareca.scrollIntoView();
+}
+function enviarMensagem(botaoEnviar){
+    const rodape = botaoEnviar.parentNode;
+    let texto = rodape.children[0].value;
+    let mensagem = {
+        from: nome.name,
+        to: "Todos",
+        text: texto,
+        type: "message"
+    }
+    axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem);
+    rodape.children[0].value = '';
 }
